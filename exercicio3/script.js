116296c846c4483a452afb464480c2a692a4861f
Maxuel Lima <maxuellima20@gmail.com>
2023-05-11 00:04:50 -0300

    Exercícios de Strings feitos individualmente

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,8 @@
-//Crie a const para a frase aqui
+const frase = 'Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"'
+
+const fraseNova = frase.replace("verde", "rosa").replace("azul","laranja")
+const verificaFrase = fraseNova.includes("verde" || "laranja")
+const fraseMaiuscula = fraseNova.replaceAll("mas não deixe o gato sair", "MAS NÃO DEIXE O GATO SAIR")
+console.log(fraseNova)
+console.log(verificaFrase)
+console.log(fraseMaiuscula)
