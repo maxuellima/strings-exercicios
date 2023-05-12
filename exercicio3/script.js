4ba8f4674fa19a0267c0b18230a81c31160d0b40
Maxuel Lima <maxuellima20@gmail.com>
2023-05-11 23:42:01 -0300

    Alguns ajustes no exercício

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,8 +1,9 @@
-const frase = 'Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"'
+const phrase = 'Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"'
 
-const fraseNova = frase.replace("verde", "rosa").replace("azul","laranja")
-const verificaFrase = fraseNova.includes("verde" || "laranja")
-const fraseMaiuscula = fraseNova.replaceAll("mas não deixe o gato sair", "MAS NÃO DEIXE O GATO SAIR")
-console.log(fraseNova)
-console.log(verificaFrase)
-console.log(fraseMaiuscula)
+const newPhrase = phrase.replace("verde", "rosa").replace("azul","laranja")
+const checkPhrase = newPhrase.includes("verde" && "laranja")
+const upperPhrase = "mas não deixe o gato sair"
+const upperCasePhrase = newPhrase.replace("mas não deixe o gato sair", "MAS NÃO DEIXE O GATO SAIR")
+console.log(newPhrase)
+console.log(checkPhrase)
+console.log(upperCasePhrase)
